70658621a2610903196a342a46d642541584a9a6
OlaOluwalekan <olalekanbello534@gmail.com>
2022-10-10 20:31:12 +0100

    navigation item styled

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
-// TO OPEN AND CLOSE NAV BAR
-
+// DOM ELEMENT SELECTION
 const navbar = document.querySelector(".nav-bar");
 const topbar = document.querySelector(".top-bar");
 const midbar = document.querySelector(".mid-bar");
 const bottombar = document.querySelector(".bottom-bar");
+const navItems = document.querySelector(".nav-items");
+const overlay = document.querySelector(".overlay");
 
+// let navIsClosed = true;
+
+// TO OPEN AND CLOSE NAV BAR
 let navClosed = true;
 
 navbar.onclick = () => {
@@ -14,6 +18,7 @@ navbar.onclick = () => {
       RotateBar(topbar, 45);
       RotateBar(bottombar, -45);
     }, 100);
+    OpenNav();
     navClosed = false;
   } else {
     RotateBar(topbar, 0);
@@ -21,15 +26,28 @@ navbar.onclick = () => {
     setTimeout(() => {
       ResizeBar(100);
     }, 100);
+    closeNav();
     navClosed = true;
   }
 };
 
 const RotateBar = (bar, angle) => {
   bar.style.transform = `rotate(${angle}deg)`;
-  // bar.style.width = `${width}%`;
 };
 
 const ResizeBar = (width) => {
   midbar.style.width = `${width}%`;
+};
+
+const OpenNav = () => {
+  navItems.style.top = "50px";
+  overlay.style.display = "flex";
+  document.body.style.overflow = "hidden";
+  // document.body.style.display = "none";
+};
+
+const closeNav = () => {
+  navItems.style.top = "-600px";
+  overlay.style.display = "none";
+  document.body.style.overflow = "unset";
 };
